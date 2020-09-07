bc36d3c13839c31534a002da7df46b3534344c75
Allysson Martins <martins.allysson@hotmail.com>
2020-09-07 04:23:07 -0300

    segundo commit

---
+++ SistemJogador/Jogador.c
@@ -0,0 +1,9 @@
+//
+//  Jogador.c
+//  SistemJogador
+//
+//  Created by Allysson Martins on 07/09/20.
+//  Copyright © 2020 Allysson Martins. All rights reserved.
+//
+
+#include "Jogador.h"
---
+++ SistemJogador/Jogador.h
@@ -0,0 +1,22 @@
+//
+//  Jogador.h
+//  SistemJogador
+//
+//  Created by Allysson Martins on 07/09/20.
+//  Copyright © 2020 Allysson Martins. All rights reserved.
+//
+/*
+#ifndef Jogador_h
+#define Jogador_h
+
+#include <stdio.h>
+
+#endif  Jogador_h */
+
+
+typedef struct {
+    int codigo;
+    int jogos;
+    int gols;
+    int assistencias;
+}Jogador;
