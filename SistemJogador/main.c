54ff2e219d26d0a5fefe8a5ce5111f0e3d3640f5
Allysson Martins <martins.allysson@hotmail.com>
2020-09-07 04:06:41 -0300

    primeiro commit

---
+++ SistemJogador/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SistemJogador
+//
+//  Created by Allysson Martins on 07/09/20.
+//  Copyright © 2020 Allysson Martins. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, mundo!\n");
+    return 0;
+}
